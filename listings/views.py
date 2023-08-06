8da0cc67ec3abc4a7275ef3e0437fff3905d2c66
nick6850 <137728530+nick6850@users.noreply.github.com>
2023-08-07 02:28:07 +0300

    Partially fixed search template
    Added filters based on the dictionary of choices for filtering by number of bedrooms, state, and price. Search functionality still needs to be implemented, but the filters now display correctly.

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Listing
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .choices import price_choices, bedroom_choices, state_choices
+
 
 # Create your views here.
 def index(request):
@@ -23,4 +25,12 @@ def listing(request, listing_id):
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+        
+    context = {
+        'price_choices': price_choices, 
+        'bedroom_choices':bedroom_choices, 
+        'state_choices': state_choices
+    }
+    
+
+    return render(request, 'listings/search.html', context)
