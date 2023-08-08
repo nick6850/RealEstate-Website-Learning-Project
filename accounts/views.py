9ecfb8f6b8a5e4af0975199fc66fb9e66c8ef88b
nick6850 <137728530+nick6850@users.noreply.github.com>
2023-08-09 02:32:47 +0300

    Implement login functionality and user authentication
    Users can now provide their credentials on the login page, which are then verified against the database. If the provided credentials match an existing user, they are successfully logged in. Additionally, the logout feature has been implemented, allowing users to securely log out of their accounts. These updates ensure a seamless and secure login experience for our users.

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.contrib import messages
+from django.contrib import auth
 from django.contrib.auth.models import User
 
 # Create your views here.
@@ -33,12 +34,26 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-        pass
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid Credentials')
+            redirect('login')
+        
     return render(request, 'accounts/login.html')
 
 
 def logout(request):
-    return redirect('index')
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'You are now logged out')
+        return redirect('index')
 
 
 def dashboard(request):
