ce2b3662b07b324eb6eb7be994d090995289a00c
nick6850 <137728530+nick6850@users.noreply.github.com>
2023-08-08 20:03:44 +0300

    Add backend functionality to register page
    This commit adds the necessary backend functionality to the register page, allowing users to register by inputting correct data. The registration data is now securely stored in the database for future use.

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        pass
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email is being used')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+                    user.save()
+                    messages.success(request, 'You are now registered and can log in')
+                    redirect('login')
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
     return render(request, 'accounts/register.html')
 
 
--- my_project/settings.py
+++ my_project/settings.py
@@ -134,3 +134,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA = 'media/'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
